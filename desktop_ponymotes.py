719ff4e7ecdd4da68e58641eac9b28d8be0d9aab
Jibodeah <Jibodeah@gmail.com>
2015-11-08 23:00:04 +0000

    Script can now be made executable and work on distros without unified /usr

--- desktop_ponymotes.py
+++ desktop_ponymotes.py
@@ -1,4 +1,4 @@
-#!/bin/env python3
+#!/usr/bin/env python3
 
 import sqlite3
 import os
@@ -10,7 +10,7 @@ import configparser
 import coloredlogs
 import requests
 
-__version__ = "1.0"
+__version__ = "1.0.1"
 
 # Should auto set itself up on first run.
 
